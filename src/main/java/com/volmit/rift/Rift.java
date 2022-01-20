e721191781d10e5ab96a69eb28acb246470eb4b8
cyberpwn <danielmillst@gmail.com>
2022-01-20 07:14:15 -0500

    Main class

---
+++ src/main/java/com/volmit/rift/Rift.java
@@ -0,0 +1,15 @@
+package com.volmit.rift;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Rift extends JavaPlugin {
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
